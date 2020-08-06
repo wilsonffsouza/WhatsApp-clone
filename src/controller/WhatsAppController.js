6d9a9bb57336715b415df7c979e3b1b82e57466e
Wilson Souza <WilsonffSouza@gmail.com>
2020-08-06 14:58:58 -0300

    whatsApp-Clone v1.0.1

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -255,6 +255,10 @@ export class WhatsAppController {
 
             if (this.el.inputDocument.files.length) {
 
+                this.el.panelDocumentPreview.css({
+                    'height': '1%'
+                });
+
                 let file = this.el.inputDocument.files[0];
 
                 this._documentPreviewController = new DocumentPreviewController(file);
@@ -266,7 +270,16 @@ export class WhatsAppController {
                     this.el.imagePanelDocumentPreview.show();
                     this.el.filePanelDocumentPreview.hide();
 
+                    this.el.panelDocumentPreview.css({
+                        'height': 'calc(100% - 120px)'
+                    });
+
                 }).catch(err => {
+
+                    this.el.panelDocumentPreview.css({
+                        'height': 'calc(100% - 120px)'
+                    });
+
                     switch (file.type) {
 
                         case 'application/vnd.ms-excel':
